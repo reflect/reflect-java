13fc904b1c051f07db24b90e65bc11e271f0fd27
lucperkins <lucperkins@gmail.com>
2017-04-05 10:56:47 -0700

    remove Main class

--- src/main/java/io/reflect/token/Main.java
+++
@@ -1,8 +0,0 @@
-package io.reflect.token;
-
-public class Main {
-    public static void main(String[] args) {
-        Parameter regionParam = new Parameter("Region", Parameter.Op.EQUALS, "Northwest");
-        System.out.println(TokenGenerator.generate("a1b2c3", new Parameter[]{regionParam}));
-    }
-}
